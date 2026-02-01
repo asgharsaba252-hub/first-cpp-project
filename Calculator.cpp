8bd7c7e2dc31b0950bfbaab656f1c488dd4baf61
Saba Asghar <asgharsaba252@gmail.com>
2026-02-01 17:07:07 -0600

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a = 4;
+  int b = 7;
+  cout << "a + b = " << a + b << endl;
+  cout << "a - b = " << a - b << endl;
+  cout << "a * b = " << a * b << endl;
+  cout << "a / b = " << (double)a / b << endl;
+  return 0;
+}
